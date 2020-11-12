9ec6b22cd6dae9f0c0f41021e06da02a427ea1cb
nikatheengineer <npest17@freeuni.edu.ge>
2020-11-12 22:59:40 +0400

    Add player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,18 +4,55 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [Range(0, 1)]
-    public float speed;
+    [Range(0, 10)]
+    public float speed = 10;
+    [Range(0, 100)]
+    public float force = 100;
+
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+    private bool lookinLeft = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        transform.up = new Vector2(0, 0);
     }
-    
+
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    private void FixedUpdate()
+    {
+        float forceX = 0, forceY = 0;
+        if (Input.GetKey(KeyCode.DownArrow) && rb.velocity.y > -speed)
+        {
+            forceY -= force;
+        }
+        if (Input.GetKey(KeyCode.UpArrow) && rb.velocity.y < speed)
+        {
+            forceY += force;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) && rb.velocity.x > -speed)
+        {
+            lookinLeft = true;
+            forceX -= force;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) && rb.velocity.x < speed)
+        {
+            lookinLeft = false;
+            forceX += force;
+        }
+        rb.AddForce(new Vector2(forceX, forceY));
+        if (rb.velocity.y > speed) rb.velocity = new Vector2(rb.velocity.x, speed);
+        else if (rb.velocity.y < -speed) rb.velocity = new Vector2(rb.velocity.x, -speed);
+        if (rb.velocity.x > speed) rb.velocity = new Vector2(speed, rb.velocity.y);
+        else if (rb.velocity.x < -speed) rb.velocity = new Vector2(-speed, rb.velocity.y);
+        sr.flipX = lookinLeft;
     }
 }
