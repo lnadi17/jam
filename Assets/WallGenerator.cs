cc0fec7a9833e108cd27b5605bd174de2a21442f
nikatheengineer <npest17@freeuni.edu.ge>
2020-11-12 18:05:54 +0400

    Add walls on every side

--- Assets/WallGenerator.cs
+++ Assets/WallGenerator.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class WallGenerator : MonoBehaviour
 {
     public GameObject wall;
-    public string problem;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        Vector3 dimensions = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+        GameObject bottomWall = Instantiate<GameObject>(wall, new Vector3(0, -dimensions.y - 0.5f, 0), Quaternion.identity);
+        GameObject topWall = Instantiate<GameObject>(wall, new Vector3(0, dimensions.y + 0.5f, 0), Quaternion.identity);
+        GameObject leftWall = Instantiate<GameObject>(wall, new Vector3(-dimensions.x - 0.5f, 0, 0), Quaternion.identity);
+        GameObject rightWall = Instantiate<GameObject>(wall, new Vector3(dimensions.x + 0.5f, 0, 0), Quaternion.identity);
+        bottomWall.transform.localScale = new Vector3(2 * dimensions.x, 1);
+        topWall.transform.localScale = new Vector3(2 * dimensions.x, 1);
+        leftWall.transform.localScale = new Vector3(1, 2 * dimensions.y);
+        rightWall.transform.localScale = new Vector3(1, 2 * dimensions.y);
     }
 }
