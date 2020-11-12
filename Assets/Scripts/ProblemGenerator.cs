35a093f7abcc782777114ae7b9b27fdde2a13bfe
Luka Nadiradze <19193250+lnadi17@users.noreply.github.com>
2020-11-13 03:04:03 +0400

    Merge Numbers_generation into master

--- Assets/Scripts/ProblemGenerator.cs
+++ Assets/Scripts/ProblemGenerator.cs
@@ -4,49 +4,141 @@ using UnityEngine;
 
 public class ProblemGenerator : MonoBehaviour
 {
+
+    private enum Operator
+    {
+        PLUS,
+        MINUS
+    }
+
+    public static readonly int MIN_SUM = 20;
+    public static readonly int MAX_SUM = 20;
+    public static readonly int TIMER_SECS = 10;
+
     public enum Type { Arithmetic, Equation };
     public GameObject numberObject;
     public Sprite[] numberSprites;
+    public Sprite[] timerSprites;
     public Sprite plus;
     public Sprite minus;
     public Sprite equals;
     public float xOffset;
+    private List<GameObject> numbersPresentObjects;
+    private GameObject timerObject;
+    private int secondsPassed;
+    private List<Vector2> numbersPresentRangesX;
+    private List<Vector2> otherRangesX;
+    private List<Vector2> numbersPresentRangesY;
+    private List<Vector2> otherRangesY;
 
     // Start is called before the first frame update
     void Start()
     {
-        DrawArithmetic("+", AdditionProblem(20, 20));
+        numbersPresentRangesX = new List<Vector2>();
+        otherRangesX = new List<Vector2>();
+        numbersPresentRangesY = new List<Vector2>();
+        otherRangesY = new List<Vector2>();
+        numbersPresentObjects = new List<GameObject>();
+        timerObject = new GameObject("NumbersPresent");
+        secondsPassed = 0;
+        drawObjects();
+        InvokeRepeating("DrawAll", 0, 1.0f);
     }
 
-    int[] GetPossibleAnswers(string op, int firstNumber, int secondNumber, int arraySize)
+    void DrawAll()
     {
-        return null;
+        if (secondsPassed == TIMER_SECS)
+        {
+            foreach (GameObject numbersPresentObjcet in numbersPresentObjects)
+            {
+                Destroy(numbersPresentObjcet);
+            }
+            numbersPresentObjects.Clear();
+            numbersPresentRangesX.Clear();
+            numbersPresentRangesY.Clear();
+            otherRangesX.Clear();
+            otherRangesY.Clear();
+            secondsPassed = 0;
+            drawObjects();
+        } else
+        {
+            secondsPassed++;
+        }
+        drawTimer();
     }
 
-    void DrawArithmetic(string op, Vector2Int numbers)
+    void drawObjects()
     {
-        Transform parent = new GameObject("NumbersParent").transform;
+        Operator op = getRandomOperator();
+        Vector2Int numbers = new Vector2Int(0, 0);
+        switch (op)
+        {
+            case Operator.PLUS:
+                numbers = AdditionProblem(MIN_SUM, MAX_SUM);
+                break;
+            case Operator.MINUS:
+                numbers = SubtractionProblem(MIN_SUM, MAX_SUM);
+                break;
+        }
+        DrawArithmetic(op, numbers);
+
+        HashSet<int> possibleAnswers = GetPossibleAnswers(op, numbers, 3);
+        foreach (int number in possibleAnswers)
+        {
+            DrawNumber(number);
+        }
+    }
+
+    HashSet<int> GetPossibleAnswers(Operator op, Vector2Int numbers, int arraySize)
+    {
+        HashSet<int> result = new HashSet<int>();
+        int resultNum = 0;
+        switch (op) {
+            case Operator.PLUS:
+                resultNum = numbers.x + numbers.y;
+                break;
+            case Operator.MINUS:
+                resultNum = numbers.x - numbers.y;
+                break;
+        }
+        result.Add(resultNum);
+        while (result.Count != arraySize)
+        {
+            result.Add(Random.Range(resultNum - 7, resultNum + 8));
+        }
+        return result;
+    }
+
+    private Operator getRandomOperator()
+    {
+        int len = 2;
+        return (Operator) Random.Range(0, len);
+    }
+
+    void DrawArithmetic(Operator op, Vector2Int numbers)
+    {
+        GameObject numbersPresent = new GameObject("NumbersPresent");
+        Transform parent = numbersPresent.transform;
         int count = 0;
         float xPosition = 0f;
         // Draw first
         string firstStr = numbers.x.ToString();
         foreach (char ch in firstStr)
         {
-            Debug.Log(ch);
             GameObject obj = Instantiate<GameObject>(numberObject, new Vector2(xPosition, 0), Quaternion.identity, parent);
             obj.GetComponent<SpriteRenderer>().sprite = numberSprites[(int)char.GetNumericValue(ch)];
             xPosition += xOffset;
             count++;
         }
         // Draw operator
-        if (op == "+")
+        if (op == Operator.PLUS)
         {
             GameObject obj = Instantiate<GameObject>(numberObject, new Vector2(xPosition, 0), Quaternion.identity, parent);
             obj.GetComponent<SpriteRenderer>().sprite = plus;
             xPosition += xOffset;
             count++;
         }
-        if (op == "-")
+        if (op == Operator.MINUS)
         {
             GameObject obj = Instantiate<GameObject>(numberObject, new Vector2(xPosition, 0), Quaternion.identity, parent);
             obj.GetComponent<SpriteRenderer>().sprite = minus;
@@ -63,11 +155,61 @@ public class ProblemGenerator : MonoBehaviour
             count++;
 
         }
+        Debug.Log(numberSprites[0].bounds.size.x);
         // Draw equal sign
         GameObject eq = Instantiate<GameObject>(numberObject, new Vector2(xPosition, 0), Quaternion.identity, parent);
         eq.GetComponent<SpriteRenderer>().sprite = equals;
         count++;
         parent.position = new Vector2(xOffset / 2 - count * xOffset / 2, 4.5f);
+        otherRangesX.Add(new Vector2(parent.position.x, parent.position.x + count * xOffset));
+        otherRangesY.Add(new Vector2(parent.position.y, parent.position.y + numberSprites[0].bounds.size.x));
+        numbersPresentObjects.Add(numbersPresent);
+    }
+
+    void DrawNumber(int number)
+    {
+        GameObject numbersPresent = new GameObject("NumbersPresent");
+        Transform parent = numbersPresent.transform;
+        float xPosition = 0f;
+        int count = 0;
+        if (number < 0)
+        {
+            GameObject obj = Instantiate<GameObject>(numberObject, new Vector2(xPosition, 0), Quaternion.identity, parent);
+            obj.GetComponent<SpriteRenderer>().sprite = minus;
+            xPosition += xOffset;
+            count++;
+            number = -number;
+        }
+        foreach (char ch in number.ToString())
+        {
+            GameObject obj = Instantiate<GameObject>(numberObject, new Vector2(xPosition, 0), Quaternion.identity, parent);
+            obj.GetComponent<SpriteRenderer>().sprite = numberSprites[(int)char.GetNumericValue(ch)];
+            xPosition += xOffset;
+            count++;
+        }
+        parent.position = new Vector2(Random.Range(-3.7f, 3.7f - count * 0.5f), Random.Range(-4.7f, 4.75f));
+        numbersPresentRangesX.Add(new Vector2(parent.position.x, parent.position.x + count * xOffset));
+        numbersPresentRangesY.Add(new Vector2(parent.position.y, parent.position.y + numberSprites[0].bounds.size.x));
+        numbersPresentObjects.Add(numbersPresent);
+    }
+
+    void drawTimer()
+    {
+        Destroy(timerObject);
+        timerObject = new GameObject("NumbersPresent");
+        Transform parent = timerObject.transform;
+        float xPosition = 0f;
+        int count = 0;
+        foreach (char ch in (TIMER_SECS - secondsPassed).ToString())
+        {
+            GameObject obj = Instantiate<GameObject>(numberObject, new Vector2(xPosition, 0), Quaternion.identity, parent);
+            obj.GetComponent<SpriteRenderer>().sprite = timerSprites[(int)char.GetNumericValue(ch)];
+            xPosition += xOffset;
+            count++;
+        }
+        parent.position = new Vector2(xOffset / 2 - count * xOffset / 2, -4.5f);
+        otherRangesX.Add(new Vector2(parent.position.x, parent.position.x + count * xOffset));
+        otherRangesY.Add(new Vector2(parent.position.y, parent.position.y + numberSprites[0].bounds.size.x));
     }
 
     Vector2Int AdditionProblem(int minSum, int maxSum)
@@ -85,4 +227,5 @@ public class ProblemGenerator : MonoBehaviour
         int secondNumber = temp.x;
         return new Vector2Int(firstNumber, secondNumber);
     }
+
 }
