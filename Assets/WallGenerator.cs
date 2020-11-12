d3f615d37fd809a71813abb7621c7d3d7f30e4b8
Luka Nadiradze <19193250+lnadi17@users.noreply.github.com>
2020-11-12 12:38:16 +0400

    Initial commit

---
+++ Assets/WallGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallGenerator : MonoBehaviour
+{
+    public GameObject wall;
+    public string problem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+}
